591a35017d857a9f94068e373717d7740ee08cd0
Flawless <Flawless@Eliza>
2024-11-22 12:08:08 +0530

    My second commit

---
+++ firstMavenProject/src/main/java/firstMavenPackage/MavenDifference.java
@@ -0,0 +1,21 @@
+package firstMavenPackage;
+
+public class MavenDifference {
+	
+	public void sub()
+	{
+		int a=20;
+		int b=10;
+		int c=a-b;
+		System.out.println("Difference is " +c);
+	}
+
+	public static void main(String[] args) {
+		
+		MavenDifference obj=new MavenDifference();
+		obj.sub();
+		// TODO Auto-generated method stub
+
+	}
+
+}
