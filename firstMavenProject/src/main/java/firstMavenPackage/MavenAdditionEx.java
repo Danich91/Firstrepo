fdcfeb38e75067d779c35ff5e418247c1c7eb453
Flawless <Flawless@Eliza>
2024-11-22 11:59:52 +0530

    My sample commit

---
+++ firstMavenProject/src/main/java/firstMavenPackage/MavenAdditionEx.java
@@ -0,0 +1,20 @@
+package firstMavenPackage;
+
+public class MavenAdditionEx {
+
+	public void add()
+	{
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println(c);
+	}
+	public static void main(String[] args) {
+		
+		MavenAdditionEx obj=new MavenAdditionEx();
+		obj.add();
+		// TODO Auto-generated method stub
+
+	}
+
+}
